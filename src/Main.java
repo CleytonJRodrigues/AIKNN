f86c9e47526c5d25287bb4e6554e49ac3ffddbbe
Unknown <cleytonjose98@outlook.com>
2019-01-12 04:41:13 -0300

    Separação aleatória de bases adicionada.

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
+   static void cleanMatrix(int[][] confusionMatrix) {
+        for (int k = 0; k < confusionMatrix.length; k++) {
+            for (int z = 0; z < confusionMatrix.length; z++) {
+                confusionMatrix[k][z] = 0;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         File file = new File("src/Base/ChristianLira;CleytonJose;Database.arff");
@@ -26,47 +30,86 @@ public class Main {
             ReadFile.close(br, fr);
         }
 //tests
+        List<Instances> testSet; // Stats as a copy of the main base, but has instances moved for a the dataSet List, used for training
         Long instances = Instances.count;
         Long testAux = (instances * 2) / 3l;
-        int testBase = Integer.valueOf(testAux.toString()); // this is for separate 2/3 for data and 1/3 for tests
+//        int testBase = Integer.valueOf(testAux.toString()); // this is for separate 2/3 for data and 1/3 for tests
+        int instanceCount = Integer.valueOf(instances.toString());
 
 
         //Here i created an array for tests.
-        Instances vetor[] = base.subList(testBase, Integer.valueOf(instances.toString())).toArray(new Instances[Integer.valueOf(instances.toString()) - (testBase)]);
+//        Instances vetor[] = base.subList(testBase, Integer.valueOf(instances.toString())).toArray(new Instances[Integer.valueOf(instances.toString()) - (testBase)]);
 
-        //creating confusion matrix
-        // this loop is responsible for finding out, the number of classes/labels. (dance, rap...)
-        List<String> classifications = new ArrayList<>();
+        ArrayList<Instances> dataSet = new ArrayList<>();
 
-        for(Instances n: vetor) {
-            if(!classifications.contains(n.getClassification())) {
-                classifications.add(n.getClassification());
+        Random rand = new Random();
+        int newDataIndex = rand.nextInt(base.size());
+
+        double result1;
+        double result2;
+        double result3;
+
+        int testTimes = 1;
+        while (testTimes > 0) {
+
+            for (int i = 1; i < base.size() - Integer.valueOf(testAux.toString()); i++) {
+
+                testSet = new ArrayList<>(base);
+                for (int j = 0; j < testAux; j++) {  // Fills the dataSet List with the 2/3 of data
+
+                    while (dataSet.contains(base.get(newDataIndex))) {   //  Checks if a the object referred by the generated Index is already at the dataSet ArrayList.
+                        newDataIndex = rand.nextInt(base.size()); // Chooses a random index of an object to be added at the datSet ArrayList.
+                    }
+                    dataSet.add(base.get(newDataIndex));    //  Adds the object to the List used for training
+                    testSet.remove(base.get(newDataIndex));  // Removes every entry used for training, in the end becoming the 1/3 of the base used for tests.
+                }
+
+                Instances[] arrayTestSet = new Instances[testSet.size()];   //  Prepares a new array to be used as the test set array
+                testSet.toArray(arrayTestSet);  // Converts the testSet ArrayList to the Instances array to be used at KNN.Calculator
+
+                //creating confusion matrix
+                // this loop is responsible for finding out, the number of classes/labels. (dance, rap...)
+                List<String> classifications = new ArrayList<>();
+
+                for (Instances n : arrayTestSet) {
+                    if (!classifications.contains(n.getClassification())) {
+                        classifications.add(n.getClassification());
+                    }
+                }
+
+                //Creating confusion matrix
+                int[][] confusionMatrix = new int[classifications.size()][classifications.size()];
+
+                for (int k = 0; k < confusionMatrix.length; k++) {
+                    for (int z = 0; z < confusionMatrix.length; z++) {
+                        confusionMatrix[k][z] = 0;
+                    }
+                }
+
+
+                //Here i removed the reference for the instances that were added to the tests array;
+                //    base = base.subList(0, testBase);
+                Instances.count = testAux;//updating the number of instances.
+
+
+                // we added this int: distanceOption, 1 is for manhattan, 2 euclidian, 3 or other values: chebyshev
+                result2 = KNN.knnCalculator(i, dataSet, confusionMatrix, 2, arrayTestSet);
+                System.out.printf("Euclidian|\n Taxa de acerto: %.4f\nTaxa de erro: %.4f\n", result2, (100 - result2));
+                cleanMatrix(confusionMatrix);
+
+                result1 = KNN.knnCalculator(i, dataSet, confusionMatrix, 1, arrayTestSet);
+                System.out.printf("Manhattan|\n Taxa de acerto: %.4f\nTaxa de erro: %.4f\n", result1, (100 - result1));
+                cleanMatrix(confusionMatrix);
+
+                result3 = KNN.knnCalculator(i, dataSet, confusionMatrix, 3, arrayTestSet);
+                System.out.printf("chebyshev|\n Taxa de acerto: %.4f\nTaxa de erro: %.4f\n", result3, (100 - result3));
+
+                testSet.clear();
+                dataSet.clear();
             }
+
+            testTimes--;
         }
-
-
-        //Creating confusion matrix
-        int[][] confusionMatrix = new int [classifications.size()][classifications.size()];
-
-        for(int i = 0; i < confusionMatrix.length; i++) {
-            for(int j = 0; j< confusionMatrix.length; j++) {
-                confusionMatrix[i][j] = 0;
-            }
-        }
-
-
-
-
-        //Here i removed the reference for the instances that were added to the tests array;
-        base = base.subList(0, testBase);
-        Instances.count = testAux;//updating the number of instances.
-
-
-
-        // we added this int: distanceOption, 1 is for manhattan, 2 euclidian, 3 or other values: chebyshev
-        double result = KNN.knnCalculator(26, base, confusionMatrix, 2, vetor);
-        System.out.printf("Taxa de acerto: %.4f\nTaxa de erro: %.4f", result, (100 - result));
-
 
     }
 }
