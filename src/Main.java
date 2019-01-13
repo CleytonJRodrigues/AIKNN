2176389710a639961de9095ebdc20baec50ebd23
Unknown <cleytonjose98@outlook.com>
2019-01-13 01:05:08 -0300

    Classe Main modificada para mostrar dados de teste

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class Main {
 
-   static void cleanMatrix(int[][] confusionMatrix) {
+   static private void cleanMatrix(int[][] confusionMatrix) {
         for (int k = 0; k < confusionMatrix.length; k++) {
             for (int z = 0; z < confusionMatrix.length; z++) {
                 confusionMatrix[k][z] = 0;
@@ -49,10 +49,24 @@ public class Main {
         double result2;
         double result3;
 
-        int testTimes = 1;
+        ArrayList<Double> taxasAcertosM = new ArrayList<>();
+        ArrayList<Double> taxasAcertosE = new ArrayList<>();
+        ArrayList<Double> taxasAcertosC = new ArrayList<>();
+
+        double[] kIndexesM = new double[instanceCount];
+        double[] kIndexesE = new double[instanceCount];
+        double[] kIndexesC = new double[instanceCount];
+
+        ArrayList<Double[]> betterResultsM = new ArrayList<>();
+        ArrayList<Double[]> betterResultsE = new ArrayList<>();
+        ArrayList<Double[]> betterResultsC = new ArrayList<>();
+
+        double[][] meansOfK = new double[27][3];
+
+        int testTimes = 50;
         while (testTimes > 0) {
 
-            for (int i = 1; i < base.size() - Integer.valueOf(testAux.toString()); i++) {
+            for (int i = 1; i < 28; i++) {
 
                 testSet = new ArrayList<>(base);
                 for (int j = 0; j < testAux; j++) {  // Fills the dataSet List with the 2/3 of data
@@ -93,23 +107,102 @@ public class Main {
 
 
                 // we added this int: distanceOption, 1 is for manhattan, 2 euclidian, 3 or other values: chebyshev
-                result2 = KNN.knnCalculator(i, dataSet, confusionMatrix, 2, arrayTestSet);
-                System.out.printf("Euclidian|\n Taxa de acerto: %.4f\nTaxa de erro: %.4f\n", result2, (100 - result2));
+                result1 = KNN.knnCalculator(i, dataSet, confusionMatrix, 1, arrayTestSet);
+                System.out.printf("\nManhattan|\n Taxa de acerto: %.4f\nTaxa de erro: %.4f\n", result1, (100 - result1));
                 cleanMatrix(confusionMatrix);
 
-                result1 = KNN.knnCalculator(i, dataSet, confusionMatrix, 1, arrayTestSet);
-                System.out.printf("Manhattan|\n Taxa de acerto: %.4f\nTaxa de erro: %.4f\n", result1, (100 - result1));
+                result2 = KNN.knnCalculator(i, dataSet, confusionMatrix, 2, arrayTestSet);
+                System.out.printf("\nEuclidean|\n Taxa de acerto: %.4f\nTaxa de erro: %.4f\n", result2, (100 - result2));
                 cleanMatrix(confusionMatrix);
 
                 result3 = KNN.knnCalculator(i, dataSet, confusionMatrix, 3, arrayTestSet);
-                System.out.printf("chebyshev|\n Taxa de acerto: %.4f\nTaxa de erro: %.4f\n", result3, (100 - result3));
+                System.out.printf("\nChebyshev|\n Taxa de acerto: %.4f\nTaxa de erro: %.4f\n", result3, (100 - result3));
 
                 testSet.clear();
                 dataSet.clear();
+
+                taxasAcertosM.add(result1);
+                kIndexesM[i] = result1;
+
+                taxasAcertosE.add(result2);
+                kIndexesE[i] = result2;
+
+                taxasAcertosC.add(result3);
+                kIndexesC[i] = result3;
+
+                meansOfK[i-1][0] += result1;
+                meansOfK[i-1][1] += result2;
+                meansOfK[i-1][2] += result3;
             }
 
-            testTimes--;
-        }
+            taxasAcertosM.sort((a1, a2) -> a1.compareTo(a2));
+            taxasAcertosE.sort((a1, a2) -> a1.compareTo(a2));
+            taxasAcertosC.sort((a1, a2) -> a1.compareTo(a2));
 
-    }
+
+            Double melhorTaxaM;
+            Double melhorTaxaE;
+            Double melhorTaxaC;
+
+            int numberOfKs = 4;
+            while(numberOfKs > 0) {
+
+                melhorTaxaM = taxasAcertosM.get(taxasAcertosM.size() - ((4 - numberOfKs) + 1));
+                melhorTaxaE = taxasAcertosE.get(taxasAcertosE.size() - ((4 - numberOfKs) + 1));
+                melhorTaxaC = taxasAcertosC.get(taxasAcertosC.size() - ((4 - numberOfKs) + 1));
+
+                Double[] betterResultM = new Double[2];
+                Double[] betterResultE = new Double[2];
+                Double[] betterResultC = new Double[2];
+
+                for (int i = 1; i < 28; i++) {
+                    if (melhorTaxaM == kIndexesM[i]) {
+                        betterResultM[0] = ((double) i);
+                        betterResultM[1] = melhorTaxaM;
+                    }
+                    if (melhorTaxaE == kIndexesE[i]) {
+                        betterResultE[0] = ((double) i);
+                        betterResultE[1] = melhorTaxaE;
+                    }
+                    if (melhorTaxaC == kIndexesC[i]) {
+                        betterResultC[0] = ((double) i);
+                        betterResultC[1] = melhorTaxaC;
+                    }
+                }
+
+                if(betterResultM[0]!=null) betterResultsM.add(betterResultM);
+                if(betterResultE[0]!=null) betterResultsE.add(betterResultE);
+                if(betterResultC[0]!=null) betterResultsC.add(betterResultC);
+
+                numberOfKs--;
+            }
+            testTimes--;
+
+            Comparator<Double[]> comparator = new Comparator<Double[]>() {
+                @Override
+                public int compare(Double[] o1, Double[] o2) {
+                    if(o1[1] == null || o2[1] == null) return -1;
+                    return Double.compare(o1[1], o2[1]);
+                }
+            };
+
+            betterResultsM.sort(comparator.reversed());
+            betterResultsE.sort(comparator.reversed());
+            betterResultsC.sort(comparator.reversed());
+
+            for(int i = 0; i < 4; i++)
+                System.out.printf("\nMelhor K Manhattan: %.4f Taxa de Acerto: %f", betterResultsM.get(i)[0], betterResultsM.get(i)[1]);
+            for(int i = 0; i < 4; i++)
+                System.out.printf("\nMelhor K Euclidean: %.4f Taxa de Acerto: %f", betterResultsE.get(i)[0], betterResultsE.get(i)[1]);
+            for(int i = 0; i < 4; i++)
+                System.out.printf("\nMelhor K Chebyschev: %.4f Taxa de Acerto: %f", betterResultsC.get(i)[0], betterResultsC.get(i)[1]);
+        }
+        testTimes = 50;
+
+        for(int i = 0; i < 27; i++) {
+            System.out.println("\nMédia de taxas de acerto para distância Manhattan com K = " + (i+1) +": " + ((meansOfK[i][0])/ testTimes));
+            System.out.println("Média de taxas de acerto para distância Euclidean com K = " + (i+1) +": " + ((meansOfK[i][1])/ testTimes));
+            System.out.println("Média de taxas de acerto para distância Chebyschev com K = "+ (i+1) +": " + ((meansOfK[i][2])/ testTimes));
+        }
+   }
 }
